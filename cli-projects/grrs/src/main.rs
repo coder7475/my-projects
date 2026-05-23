fe2a12e3515573fb827b8e51535f52856ce80c68
coder7475 <robiulhossain7475@gmail.com>
2026-05-23 16:05:29 +0600

    feat: first implementation of grrs cli tool

---
+++ cli-projects/grrs/src/main.rs
@@ -0,0 +1,20 @@
+use clap::Parser;
+
+// Search for a pattern in a file and display the lines that contain in
+#[derive(Parser)]
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
+}
+
+fn main() {
+    let args = Cli::parse();
+    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{line}");
+        }
+    }
+
+}
