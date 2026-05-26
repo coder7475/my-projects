ab40e2f385f1ec2f160f7eb177767afce5ecb569
coder7475 <robiulhossain7475@gmail.com>
2026-05-26 17:43:23 +0600

    feat(grrs): better error reporting

--- cli-projects/grrs/src/main.rs
+++ cli-projects/grrs/src/main.rs
@@ -1,3 +1,4 @@
+use anyhow::{Context, Result};
 use clap::Parser;
 
 // Search for a pattern in a file and display the lines that contain in
@@ -7,9 +8,10 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() {
+fn main() -> Result<()> {
     let args = Cli::parse();
-    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+    let content = std::fs::read_to_string(&args.path)
+        .with_context(|| format!("could not read file `{}`", args.path.display()))?;
 
     for line in content.lines() {
         if line.contains(&args.pattern) {
@@ -17,4 +19,5 @@ fn main() {
         }
     }
 
+    Ok(())
 }
