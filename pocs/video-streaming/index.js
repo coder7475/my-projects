f7668a1281bd3ee0823ab586462c91ac616600d9
coder7475 <robiulhossain7475@gmail.com>
2026-06-08 15:57:04 +0600

    feat(video-streaming): configure multer

--- pocs/video-streaming/index.js
+++ pocs/video-streaming/index.js
@@ -2,8 +2,20 @@ import express from "express";
 import cors from "cors";
 import multer from "multer";
 import { v4 as uuidv4 } from "uuid";
+import path from "node:path";
 
 const app = express();
+const storage = multer.diskStorage({
+  destination: function (req, res, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + "-" + uuidv4() + path.extname(file.originalname));
+  }
+})
+
+const multer = upload({ storage });
+
 
 app.use(cors({
   origin: ["http://localhost:3000", "http://localhost:5173"],
@@ -11,9 +23,15 @@ app.use(cors({
 }))
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "*");// remove in production
+  res.header("Access-Control-Allow-Headers",
+    "Origin", "X-Requested-With", "Content-Type", "Accept")
   next();
 })
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/", express.static("/uploads"));
 
 app.get("/", (_req, res) => {
   res.json({
